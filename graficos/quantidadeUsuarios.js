f310c89d54daf655a121b42810842cec2b95d842
Henrique_Luan <105437749+h-luan2006@users.noreply.github.com>
2024-12-06 07:44:12 -0300

    save684

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -4,12 +4,12 @@ async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
+    const nomeDosEsportes = Object.keys(dados)
     const quantidadeDeUsuarios = Object.values(dados)
 
     const data = [
         {
-            x: nomeDasRedes,
+            x: nomeDosEsportes,
             y: quantidadeDeUsuarios,
             type: 'bar',
             marker: {
